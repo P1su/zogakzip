2e35720d2918422967e10560d271ee40fdda3b05
P1su <pissssssu@naver.com>
2024-09-18 20:42:28 +0900

    feat: 버튼 필드 생성 후 버튼 컴포넌트 배치

--- src/pages/Start/Start.style.ts
+++ src/pages/Start/Start.style.ts
@@ -2,10 +2,16 @@ import styled from 'styled-components';
 
 export const StartWrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
-  ${({ theme }) => theme.fonts.title};
   width: 100%;
   height: 100vh;
 `;
+export const ButtonField = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'flex-end' })};
+  width: 100%;
+  position: absolute;
+  top: 2.7rem;
+  padding-right: 18rem;
+`
 export const Text = styled.p`
   ${({ theme }) => theme.fonts.font_06};
 `;
--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,8 +1,15 @@
+import BtnSmall from '../../components/button/SmallButton/BtnSmall';
 import * as S from './Start.style';
 
 const Start = () => {
   return(
     <S.StartWrapper>
+      <S.ButtonField>
+        <BtnSmall onClick={() => alert('')}>
+          그룹 만들기
+        </BtnSmall>
+      </S.ButtonField>
+
       시작 페이지입니다
     </S.StartWrapper>
   );
