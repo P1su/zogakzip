a981aa9cd271903b9f8e0672d5eb3c416857efc9
P1su <pissssssu@naver.com>
2024-09-18 15:56:15 +0900

    chore: QueryClientProvider 태그 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './router/Router'
+import router from './router/Router';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </>
-  )
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
