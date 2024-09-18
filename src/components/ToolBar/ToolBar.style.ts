8e3b5e621ac43c450575d8e7b6992adf74922f5d
P1su <pissssssu@naver.com>
2024-09-18 20:57:27 +0900

    feat: ToolBar 컴포넌트 생성

---
+++ src/components/ToolBar/ToolBar.style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ToolBarWrapper = styled.div`
+  width: 100%;
+`;
---
+++ src/components/ToolBar/ToolBar.tsx
@@ -0,0 +1,11 @@
+import * as S from './ToolBar.style';
+
+const ToolBar = () => {
+  return(
+    <S.ToolBarWrapper>
+
+    </S.ToolBarWrapper>
+  );
+};
+
+export default ToolBar;
