747a07fe65189fcceb7731891c6708e796cf186b
P1su <pissssssu@naver.com>
2024-09-21 22:33:12 +0900

    feat: 그룹 생성 페이지로 이동하는 버튼 클릭 이벤트 핸들러 생성 후 적용

--- src/components/Group/GroupList.tsx
+++ src/components/Group/GroupList.tsx
@@ -3,10 +3,16 @@ import * as S from './GroupList.style';
 import nonGroup from '../../assets/img/nonGroup.png';
 import BtnLarge from '../button/LargeButton/BtnLarge';
 import PublicGroupItem from './PublicGroupItem/PublicGroupItem';
+import { useNavigate } from 'react-router-dom';
 
 const GroupList = () => {
   //api연결
   const data = groupMockData;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('/create-group');
+  };
 
   return(
     <S.GroupListWrapper>
@@ -26,7 +32,7 @@ const GroupList = () => {
           :
             <S.NonGroupBox>
               <S.NonGroupImage src={nonGroup} />
-              <BtnLarge onClick={() => {alert('dd')}}>그룹 만들기</BtnLarge>
+              <BtnLarge onClick={() => {handleNavigate()}}>그룹 만들기</BtnLarge>
             </S.NonGroupBox>
       }
     </S.GroupListWrapper>
