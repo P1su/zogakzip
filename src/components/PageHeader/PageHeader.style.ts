5f0b0e1b60daf59450d5f7b392ee7654d47234ac
P1su <pissssssu@naver.com>
2024-09-24 17:39:13 +0900

    style: 페이지 헤더 display 스타일 변경

--- src/components/PageHeader/PageHeader.style.ts
+++ src/components/PageHeader/PageHeader.style.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const PageHeaderWrapper = styled.div`
-  ${({ theme: { mixin }}) => mixin.flexBox({ justify: 'space-between', align: 'center' })};
+  ${({ theme: { mixin }}) => mixin.flexBox({ justify: 'center', align: 'center' })};
   width: 100%;
   height: 10rem;
 `;
 export const IconField = styled.div`
-  margin-left: 50%;
+  position: fixed;
 `;
 export const ButtonField = styled.div`
-  padding-right: 40rem;
+  padding-left: 135rem;
 `;
