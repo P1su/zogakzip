9d2c6d1a6cae97bd6840828b3997df4ffbab352a
P1su <pissssssu@naver.com>
2024-09-26 18:50:46 +0900

    feat: 추억 생성 페이지 생성 후 라우트 연결

---
+++ src/pages/CreateMemory/CreateMemory.tsx
@@ -0,0 +1,11 @@
+import * as S from './CreateMemory.style';
+
+const CreateMemory = () => {
+  return(
+    <S.CreateMemoryWrapper>
+      추억 생성 페이지
+    </S.CreateMemoryWrapper>
+  );
+};
+
+export default CreateMemory;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import Main from '../pages/Main/Main';
 import Layout from '../layouts/Layout';
 import CreateGroup from '../pages/CreateGroup/CreateGroup';
 import Group from '../pages/Group/Group';
+import CreateMemory from '../pages/CreateMemory/CreateMemory';
 
 const router = createBrowserRouter([
   {
@@ -16,9 +17,14 @@ const router = createBrowserRouter([
       {
         path: '/create-group',
         element: <CreateGroup />,
-      },{
+      },
+      {
         path: '/group/:groupId',
         element: <Group />,
+      },
+      {
+        path: '/create-memory',
+        element: <CreateMemory />,
       },
     ],
   },
