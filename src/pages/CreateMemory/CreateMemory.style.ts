69474a2a7b9e4a48492469d9fde1114b01a54de0
P1su <pissssssu@naver.com>
2024-09-30 02:22:02 +0900

    feat: 메모리 폼 컴포넌트 생성 후 적용

--- src/pages/CreateMemory/CreateMemory.style.ts
+++ src/pages/CreateMemory/CreateMemory.style.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const CreateMemoryWrapper = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
   width: 100%;
   height: 100vh;
+  gap: 10rem;
 `;
 export const TitleText = styled.span`
   ${({ theme }) => theme.fonts.title};
--- src/pages/CreateMemory/CreateMemory.tsx
+++ src/pages/CreateMemory/CreateMemory.tsx
@@ -1,9 +1,17 @@
+import MemoryForm from '../../components/MemoryForm/MemoryForm';
 import * as S from './CreateMemory.style';
+import BtnLarge from '../../components/button/LargeButton/BtnLarge';
 
 const CreateMemory = () => {
+  const handleClick = () => {
+    alert('저장 성공');
+  };
+
   return(
     <S.CreateMemoryWrapper>
       <S.TitleText>추억 올리기</S.TitleText>
+      <MemoryForm />
+      <BtnLarge onClick={handleClick}>추억 생성하기</BtnLarge>
     </S.CreateMemoryWrapper>
   );
 };
