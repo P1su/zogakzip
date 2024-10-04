7f1556faee089c0d7c4aac96e051d81066eaa3fa
P1su <pissssssu@naver.com>
2024-10-05 01:57:34 +0900

    feat: 추억 생성 페이지에서 닫기 버튼 누르면 그룹으로 돌아가는 기능 구현

--- src/pages/CreateMemory/CreateMemory.style.ts
+++ src/pages/CreateMemory/CreateMemory.style.ts
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 export const CreateMemoryWrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
   width: 100%;
-  height: 100vh;
-  gap: 10rem;
+  padding: 6rem 0 6rem 0;
+`;
+export const IconField = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'flex-end' })};
+  width: 110rem;
+  cursor: pointer;
 `;
 export const TitleText = styled.span`
   ${({ theme }) => theme.fonts.title};
--- src/pages/CreateMemory/CreateMemory.tsx
+++ src/pages/CreateMemory/CreateMemory.tsx
@@ -1,14 +1,23 @@
+import { IcDelete } from '../../assets/svg';
 import MemoryForm from '../../components/MemoryForm/MemoryForm';
 import * as S from './CreateMemory.style';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const CreateMemory = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const groupId = location.state.id;
   console.log(groupId);
 
+  const handleNavigate = () => {
+    navigate(`/group/${groupId}`, {state: {groupId: groupId}});
+  };
+  
   return(
     <S.CreateMemoryWrapper>
+      <S.IconField onClick={() => handleNavigate()}>
+        <IcDelete />
+      </S.IconField>
       <S.TitleText>추억 올리기</S.TitleText>
       <MemoryForm groupId={groupId} />
     </S.CreateMemoryWrapper>
