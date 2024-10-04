0622071c557e94119371cad833e4dd7f269f1b4c
P1su <pissssssu@naver.com>
2024-10-04 23:03:58 +0900

    style: 페이지 헤더의 margin 과 justify 조정하여 버튼 여부와 상관 없이  로고를 가운데 위치

--- src/components/PageHeader/PageHeader.style.ts
+++ src/components/PageHeader/PageHeader.style.ts
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 export const PageHeaderWrapper = styled.div`
-  ${({ theme: { mixin }}) => mixin.flexBox({ justify: 'center', align: 'center' })};
-  width: 100%;
+  ${({ theme: { mixin }}) => mixin.flexBox({ justify: 'space-between', align: 'center' })};
+  width: 50%;
   height: 10rem;
+  margin-left: 48%;
 `;
 export const IconField = styled.div`
   position: relative;
   cursor: pointer;
 `;
 export const ButtonField = styled.div`
-  padding-left: 135rem;
+  padding-right: 40rem;
 `;
